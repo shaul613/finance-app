c605dc3bb2d413f7dc57ab253a05ed4dd2c50441
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-08-26 11:49:09 -0400

    final commit for friday

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,43 @@
 import {useState, useEffect} from 'react';
 
 const Home = (props) => {
+  const [randomCards, setRandomCards] = useState([]);
+
+  // const getRandomThree = (productsArr) => {
+  //   let threeRandomProducts = [];
+  //   const arrLength = productsArr.length;
+  //   for(let i=0; i<3 && i<arrLength; i++){
+  //     const randomIndex = Math.floor(Math.random()*productsArr.length);
+  //     const item = productsArr[randomIndex];
+  //     threeRandomProducts.push(item);
+  //     productsArr.splice(randomIndex);
+  //   }
+  //   console.log(threeRandomProducts);
+  //   return threeRandomProducts;
+  // }
+  //
+  // let test = [1,2,3,4,5,6,7,8];
+  // getRandomThree(test);
+  // console.log(test);
+
+  // useEffect(() => {
+  //   fetch(`/backend/products/all`)
+  //   .then(res => {
+  //     if(res.status === 200){
+  //       return res.json();
+  //     }
+  //   })
+  //   .then(data => {
+  //
+  //     setRandomCards(data);
+  //   })
+  //   .catch(e => {
+  //     console.log(e);
+  //   })
+  // },[])
   return(
     <div>
-      <h1>Home</h1>
+      <h1>Welcome to my website</h1>
     </div>
   )
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,23 +4,41 @@ import {
   Route,
   useNavigate,
 } from 'react-router-dom';
+import {useState, useEffect} from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 
 const Navbar = (props) => {
+
+  const [tabval, setTabval] = useState(0)
   return(
     <div className="navbar">
-      <Tabs centered>
-        <Stack spacing={2} direction="row" centered>
-          <Button component={Link} to='/'><Tab label="Home" /></Button>
-          <Button component={Link} to='/creditcards'><Tab label="Credit Cards" /></Button>
-          <Button component={Link} to='/crypto'><Tab label="Crypto" /></Button>
-          <Button component={Link} to='/about'><Tab label="About Us" /></Button>
-        </Stack>
+
+      <Tabs value={tabval} centered>
+        <Button component={Link} to='/'><Tab label="Home" value='0'/></Button>
+        <Button component={Link} to='/creditcards'><Tab label="Credit Cards" value='1' /></Button>
+        <Button component={Link} to='/crypto'><Tab label="Crypto" value='2' /></Button>
+        <Button component={Link} to='/about'><Tab label="About Us" value='3' /></Button>
+
+
+
+
       </Tabs>
     </div>
   )
 }
+
+// <Tab label="Home" />
+// <Tab label="Credit Cards" />
+// <Tab label="Crypto" />
+// <Tab label="About Us" />
+
+// <Stack spacing={2} direction="row">
+//   <Button component={Link} to='/'><Tab label="Home" /></Button>
+//   <Button component={Link} to='/creditcards'><Tab label="Credit Cards" /></Button>
+//   <Button component={Link} to='/crypto'><Tab label="Crypto" /></Button>
+//   <Button component={Link} to='/about'><Tab label="About Us" /></Button>
+// </Stack>
 export default Navbar;
