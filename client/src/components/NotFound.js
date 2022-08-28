da049aaecc2941ca7293576c1fea48af6ab45cc4
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-08-28 08:28:08 -0400

    added redirect and timer for 404 page

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,9 +1,34 @@
+import {
+  useNavigate,
+} from 'react-router-dom';
 
+import {
+  useEffect,
+  useState,
+} from 'react';
 
 const NotFound = (props) => {
+
+  const [time, setTime] = useState(5);
+  const navigate = useNavigate();
+
   return(
     <div>
       <h1>404: Page Not Found</h1>
+      {
+        useEffect(() => {
+
+          setTimeout(() => {
+            navigate('/');
+          }, 5000);
+
+          setInterval(() => {
+            setTime(time-1);
+          }, 1000);
+
+        })
+      }
+      <p>You'll be redirected to home page in {time} seconds</p>
     </div>
   )
 }
