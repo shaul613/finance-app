4554c111b1b8283c6b89a41b070344ae60165601
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-08-28 07:12:25 -0400

    removed buttons from nav for testing - it solves error

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,26 +5,28 @@ import {
   useNavigate,
 } from 'react-router-dom';
 import {useState, useEffect} from 'react';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import {
+  Button,
+  Stack,
+  Tabs,
+  Tab,
+} from '@mui/material';
 
 const Navbar = (props) => {
 
-  const [tabval, setTabval] = useState(0)
+  const [tabval, setTabval] = useState(0);
+
+  const handleChange = (e, val) => {
+    setTabval(val);
+  }
+
   return(
     <div className="navbar">
-
-      <Tabs value={tabval} centered>
-        <Button component={Link} to='/'><Tab label="Home" value='0'/></Button>
-        <Button component={Link} to='/creditcards'><Tab label="Credit Cards" value='1' /></Button>
-        <Button component={Link} to='/crypto'><Tab label="Crypto" value='2' /></Button>
-        <Button component={Link} to='/about'><Tab label="About Us" value='3' /></Button>
-
-
-
-
+      <Tabs value={tabval} onChange={handleChange} centered>
+        <Tab label="Home"/>
+        <Tab label="Credit Cards"/>
+        <Tab label="Crypto" />
+        <Tab label="About Us" />
       </Tabs>
     </div>
   )
@@ -41,4 +43,12 @@ const Navbar = (props) => {
 //   <Button component={Link} to='/crypto'><Tab label="Crypto" /></Button>
 //   <Button component={Link} to='/about'><Tab label="About Us" /></Button>
 // </Stack>
+
+
+// <Tabs value={tabval} onChange={handleChange} centered>
+//   <Button component={Link} to='/'><Tab label="Home"/></Button>
+//   <Button component={Link} to='/creditcards'><Tab label="Credit Cards"/></Button>
+//   <Button component={Link} to='/crypto'><Tab label="Crypto" /></Button>
+//   <Button component={Link} to='/about'><Tab label="About Us" /></Button>
+// </Tabs>
 export default Navbar;
