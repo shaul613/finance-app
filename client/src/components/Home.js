937631ad460e8dc410ac050b0ee8e1bacd5abd6a
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-08-28 10:25:04 -0400

    fixed functin for random 3

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,38 +3,42 @@ import {useState, useEffect} from 'react';
 const Home = (props) => {
   const [randomCards, setRandomCards] = useState([]);
 
-  // const getRandomThree = (productsArr) => {
-  //   let threeRandomProducts = [];
-  //   const arrLength = productsArr.length;
-  //   for(let i=0; i<3 && i<arrLength; i++){
-  //     const randomIndex = Math.floor(Math.random()*productsArr.length);
-  //     const item = productsArr[randomIndex];
-  //     threeRandomProducts.push(item);
-  //     productsArr.splice(randomIndex);
-  //   }
-  //   console.log(threeRandomProducts);
-  //   return threeRandomProducts;
-  // }
-  //
-  // let test = [1,2,3,4,5,6,7,8];
-  // getRandomThree(test);
-  // console.log(test);
+  const getRandomThree = (productsArr) => {
+    console.log(`original array ${productsArr}`);
+    let threeRandomProducts = [];
+    const arrLength = productsArr.length;
+    for(let i=0; i<3 && i<arrLength; i++){
+      const randomIndex = Math.floor(Math.random()*productsArr.length);
+      const item = productsArr[randomIndex];
+      threeRandomProducts.push(item);
+      productsArr.splice(randomIndex, 1);
+      console.log(`i == ${i}. new arr -> ${productsArr}`);
+    }
+    console.log('random three -> ', threeRandomProducts);
+    return threeRandomProducts;
+  }
 
-  // useEffect(() => {
-  //   fetch(`/backend/products/all`)
-  //   .then(res => {
-  //     if(res.status === 200){
-  //       return res.json();
-  //     }
-  //   })
-  //   .then(data => {
-  //
-  //     setRandomCards(data);
-  //   })
-  //   .catch(e => {
-  //     console.log(e);
-  //   })
-  // },[])
+
+
+  useEffect(() => {
+    let test = [1,2,3,4,5,6,7,8];
+    getRandomThree(test);
+    console.log(`whats left -> ${test}`);
+    // fetch(`/backend/products/all`)
+    // .then(res => {
+    //   if(res.status === 200){
+    //     return res.json();
+    //   }
+    // })
+    // .then(data => {
+    //
+    //   setRandomCards(data);
+    // })
+    // .catch(e => {
+    //   console.log(e);
+    // })
+  },[])
+
   return(
     <div>
       <h1>Welcome to my website</h1>
