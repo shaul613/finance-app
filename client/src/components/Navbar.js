f0d78f076c5ade202705bebc3de1d0643cd65338
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-08-28 11:44:14 -0400

    gave navbar back color and started writing db plan

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -23,15 +23,17 @@ const Navbar = (props) => {
   };
 
   return (
-    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-      <Tabs value={false} onChange={handleChange} centered>
-        {/*<Tab component={Link} to='/' label="Home"/>*/}
-        <Tab component={Link} to='/creditcards' label="Credit Cards"/>
-        <Tab component={Link} to='/crypto' label="Crypto" />
-        <Tab component={Link} to='/loans_banking' label="Loans And Banking" />
-        <Tab component={Link} to='/about' label="About Us" />
-      </Tabs>
-    </Box>
+    <div>
+      <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+        <Tabs value={false} onChange={handleChange} centered id='navbar'>
+          {/*<Tab component={Link} to='/' label="Home"/>*/}
+          <Tab component={Link} to='/creditcards' label="Credit Cards"/>
+          <Tab component={Link} to='/crypto' label="Crypto" />
+          <Tab component={Link} to='/loans_banking' label="Loans And Banking" />
+          <Tab component={Link} to='/about' label="About Us" />
+        </Tabs>
+      </Box>
+    </div>
   )
 }
 
