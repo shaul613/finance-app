87d4795523e89a054488c9fd2cc9ca4d3f8cf53b
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-08-30 09:26:13 -0400

    styled cc page

--- client/src/components/CreditCards.js
+++ client/src/components/CreditCards.js
@@ -1,4 +1,10 @@
 import {useState, useEffect} from 'react';
+import {
+  Link,
+} from 'react-router-dom';
+import {
+  Button,
+} from '@mui/material'
 
 const CreditCards = (props) => {
   const [products, setProducts] = useState([]);
@@ -22,13 +28,20 @@ const CreditCards = (props) => {
       {
         products.map(item => {
           return(
-            <div key={item.product_id} className="financial_product cc_product">
-              <h3 className="product_title cc_title"><a href='#'>{item.product_name}</a></h3>
-              <img className="product_img cc_img" src={item.product_img} alt="card-photo"/>
-              <p className="product_desc cc_desc">{item.product_desc}</p>
-              <p className="product_sub cc_sub">Sign Up Bonus: <br /> <b>{item.cc_sub}</b></p>
-              <p className="cc_score">Credit score needed (reccomended): {item.cc_score}</p>
-              <p className="product_ref cc_ref"><button><a href={item.product_ref_link} target="_blank">Sign Up</a></button></p>
+            <div key={item.product_id} className='card_item product_item'>
+            <h3 className="product_title cc_title"><a href='#'>{item.product_name}</a></h3>
+              <div className="financial_product cc_product">
+                <div className='product_title_img'>
+                  <img className="product_img cc_img" src={item.product_img} alt="card-photo"/>
+                </div>
+                <div className='card_details'>
+                  <p className="product_desc cc_desc">{item.product_desc}</p>
+                  <p className="product_sub cc_sub">Sign Up Bonus: <br /> <b>{item.cc_sub}</b></p>
+                  <p className="cc_score">Credit score needed (reccomended): {item.cc_score}</p>
+                </div>
+              </div>
+              <p className="product_ref cc_ref"><Button href={item.product_ref_link} target="_blank">Sign Up</Button></p>
+
             </div>
           )
         })
--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -4,6 +4,7 @@ const TopBar = (props) => {
   return(
     <div id='top_bar'>
       <h1>hello</h1>
+      
     </div>
   )
 }
