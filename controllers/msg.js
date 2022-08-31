0bde3463a14cbf5da974e27227bf3c798ffc79cd
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-08-31 11:28:15 -0400

    fixed likes

--- controllers/msg.js
+++ controllers/msg.js
@@ -4,6 +4,7 @@ import {
 
 export const _addLikeToMsg = (req, res) => {
   console.log('added like');
+  console.log(req.body.id);
   addLikeToMsg(req.body.id)
   .catch(e => {
     console.log(e);
--- modules/msg.js
+++ modules/msg.js
@@ -2,7 +2,8 @@ import {db} from '../connections/db.js';
 
 
 export const addLikeToMsg = (id) => {
+  console.log(id);
   return db('msg')
   .increment('msg_likes', 1)
-  .where({msg_id:id});
+  .where('msg_id', '=', id)
 };
