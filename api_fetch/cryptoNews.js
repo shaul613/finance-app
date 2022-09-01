ded409fcb15391bd45a4bfbce8a84a34f4dc7ca6
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-09-01 14:56:49 -0400

    tried with axois

--- api_fetch/cryptoNews.js
+++ api_fetch/cryptoNews.js
@@ -1,15 +1,18 @@
 import {db} from '../connections/db.js';
+import axios from "axios";
 
 export const insertCryptoNews = () => {
   const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '33a79c7126mshe2a69c62367e5abp1ecd27jsn8010148aeee9',
-		'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com'
-  	}
+    method: 'GET',
+    url: 'https://crypto-news-live3.p.rapidapi.com/news',
+    headers: {
+      'X-RapidAPI-Key': '33a79c7126mshe2a69c62367e5abp1ecd27jsn8010148aeee9',
+      'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com'
+    }
   };
-  fetch('https://crypto-news-live3.p.rapidapi.com/news', options)
-	.then(response => response.json())
+  axios.request(options).then(function (response) {
+  	console.log(response.data);
+  })
   .then(data => {
     db('crypto_news')
     .del()
--- db_insert/cryptoNews.js
+++ db_insert/cryptoNews.js
@@ -6,7 +6,7 @@ const cryptoNewsInsert = () => {
   setInterval(() => {
     console.log('inserted news!');
     insertCryptoNews()
-  }, 1000 * 60)
+  }, 1000 * 10)
 }
 
 export default cryptoNewsInsert;
