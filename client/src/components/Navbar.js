753557fb4996648a52ffb707bd6de82bc0834958
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-09-01 12:25:03 -0400

    made changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -25,15 +25,18 @@ const Navbar = (props) => {
   return (
     <div>
       <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-        <Tabs value={false} onChange={handleChange} centered id='navbar'>
+        <div id='navbar'>
+        <Tabs value={false} onChange={handleChange} centered>
           <Tab component={Link} to='/' label="Home"/>
           <Tab component={Link} to='/creditcards' label="Credit Cards"/>
           <Tab component={Link} to='/crypto' label="Crypto" />
           {/*<Tab component={Link} to='/loans_banking' label="Loans And Banking" />*/}
-          {/*<Tab component={Link} to='/feedback' label="Feedback And Comments" />*/}
+          <Tab component={Link} to='/feedback' label="Feedback And Comments" />
           <Tab component={Link} to='/about' label="About" />
-          <Tab component={Link} to='/cryptonews' label="Crypto News (coming soon!)" />
+          {/*<Tab component={Link} to='/cryptonews' label="Crypto News (coming soon!)" />*/}
         </Tabs>
+        </div>
+
       </Box>
     </div>
   )
