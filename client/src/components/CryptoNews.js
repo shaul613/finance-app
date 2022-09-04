3e510a86dbaefb90c94afc95f2c449fc9c6b145f
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-09-04 06:22:47 -0400

    crypto news

--- client/src/components/CryptoNews.js
+++ client/src/components/CryptoNews.js
@@ -6,22 +6,38 @@ import CryptoNewsItem from './CryptoNewsItem';
 
 const CryptoNews = (props) => {
   const [news, setNews] = useState([]);
+  const [rand20, setRand20] = useState([]);
 
   useEffect(() => {
     fetch('/backend/cryptonews/allcryptonews')
     .then(res => res.json())
     .then(data => {
       setNews(data);
+      let newArr = [];
+      for(let i=0; i<20; i++){
+        const nom = Math.floor(Math.random()*data.length);
+        const item = data[nom];
+        newArr.push(item);
+        data.splice(nom, 1);
+      }
+      setRand20(newArr);
+      console.log(rand20);
     })
   }, [])
+
   return(
-    <div>
+    <div id='crypto_news'>
       {
-        news.map(item => {
+        rand20.map(item => {
           return(
-            <div key={item.news_id}>
-              <CryptoNewsItem title={item.title} url={item.url}/>
-            </div>
+
+              <CryptoNewsItem
+                key={item.news_id}
+                title={item.title}
+                url={item.url}
+                source={item.source}
+              />
+
           )
         })
       }
--- client/src/components/CryptoNewsItem.js
+++ client/src/components/CryptoNewsItem.js
@@ -7,13 +7,15 @@ const CryptoNewsItem = (props) => {
   const navigate = useNavigate();
 
   // const handleClick = () => {
-  //   navigate(props.url)
+  //   // navigate(props.url)
+  //   window.location.herf = props.url;
   // }
 
   return(
     <div className='news_item'>
-      <a href={props.url}><button>
-        <h3>{props.title}</h3>
+      <a href={props.url} target='_blank' className='news_link'><button className='news_button'>
+        <h2 className='news_title'>{props.title}</h2>
+        <small>{props.source}</small>
       </button></a>
     </div>
   )
