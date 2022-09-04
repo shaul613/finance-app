7ebd423838eee9b75308eb5821346dbbd03b4557
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-09-04 14:24:28 -0400

    added active tab to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = (props) => {
     <div>
       <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
         <div id='navbar'>
-        <Tabs value={false} onChange={handleChange} centered>
+        <Tabs value={value} onChange={handleChange} centered>
           <Tab component={Link} to='/' label="Home"/>
           <Tab component={Link} to='/creditcards' label="Credit Cards"/>
           <Tab component={Link} to='/crypto' label="Crypto" />
@@ -42,13 +42,13 @@ const Navbar = (props) => {
         </Tabs>
         </div>
       </Box>
-      <div id='navbar_two'>
+      {/*<div id='navbar_two'>
         <button className='nav_item' onClick={()=>{handleClick('')}}>Home</button>
         <button className='nav_item' onClick={()=>{handleClick('creditcards')}}>Credit Cards</button>
         <button className='nav_item' onClick={()=>{handleClick('crypto')}}>Crypto</button>
         <button className='nav_item' onClick={()=>{handleClick('cryptonews')}}>Crypto News</button>
         <button className='nav_item' onClick={()=>{handleClick('about')}}>About</button>
-      </div>
+      </div>*/}
     </div>
   )
 }
