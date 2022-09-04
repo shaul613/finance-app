f7df2225b7e0da55ebcaba4d116838d9640bdf7f
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-09-04 15:07:06 -0400

    added users module. want to test on heroku

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import nc from 'node-cron';
+import cookieParser from 'cookie-parser';
 import {db} from './connections/db.js';
 import router from './routes/financialProducts.js';
 import msgRouter from './routes/msg.js';
 import cryptoNewsRouter from './routes/cryptoNews.js';
-import path from 'path';
-import nc from 'node-cron';
 import {enterCryptoNews} from './modules/cryptoNews.js';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  credentials:true,
+  origin:'http://localhost:3000'
+}));
+app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
