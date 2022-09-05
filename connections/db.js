36042d9ee94739acfe24463fa66ee89a6f188a07
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-09-05 13:23:01 -0400

    deleted comment

--- connections/db.js
+++ connections/db.js
@@ -10,4 +10,3 @@ export const db = knex({
     ssl: {rejectUnauthorized:false}
   }
 })
-// postgres://hooewudrthsuum:b7d19e92a703ad9cf9a41149560b3c35911756d5d5f83ebe599291502676db6a@ec2-107-23-76-12.compute-1.amazonaws.com:5432/d8hmo1eg35d6s5
