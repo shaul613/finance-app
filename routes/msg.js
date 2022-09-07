a4baf06e79127bc05897ba3505407666053b9df1
shaul613 <64236486+shaul613@users.noreply.github.com>
2022-09-07 02:39:29 -0400

    required log in to write comment

--- routes/msg.js
+++ routes/msg.js
@@ -11,7 +11,7 @@ const msgRouter = express.Router();
 
 msgRouter.post('/like', verifyToken, _addLikeToMsg); //liking a message
 msgRouter.get('/get_likes/:id', _getLikes); //get number of likes per message
-msgRouter.post('/add_msg', _addMsg); //add message
+msgRouter.post('/add_msg', verifyToken, _addMsg); //add message
 msgRouter.get('/all_msg', _allMsg); //get all messages
 
 export default msgRouter;
